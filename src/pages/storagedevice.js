fbe5ac33ee5874ee8b4821ac4f3872d9a9c18071
Springhawk27 <sajjad0mahmud@gmail.com>
2023-07-30 04:14:33 +0600

    storage device page implemented

---
+++ src/pages/storagedevice.js
@@ -0,0 +1,40 @@
+import RootLayout from "@/components/Layouts/RootLayout";
+import Component from "@/components/UI/Component";
+import React from "react";
+
+const RamPage = ({ specificComponents, categoryName }) => {
+  return (
+    <div>
+      <Component
+        specificComponents={specificComponents}
+        categoryName={categoryName}
+      ></Component>
+    </div>
+  );
+};
+
+export default RamPage;
+
+RamPage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:3001/api/components");
+  const data = await res.json();
+  // Get all components
+  const allComponents = data.data;
+  const categoryName = "Storage Device";
+
+  // Filter components with category
+  const specificComponents = allComponents.filter(
+    (component) => component.category === "Storage Device"
+  );
+
+  return {
+    props: {
+      specificComponents,
+      categoryName,
+    },
+  };
+};
