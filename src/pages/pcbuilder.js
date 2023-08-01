10d30189ce8e69020267741b06a7b5b62b07eb8a
Springhawk27 <sajjad0mahmud@gmail.com>
2023-08-01 06:24:56 +0600

    remove from builder implemented

--- src/pages/pcbuilder.js
+++ src/pages/pcbuilder.js
@@ -2,11 +2,14 @@ import RootLayout from "@/components/Layouts/RootLayout";
 import React from "react";
 import { Button, Col, List, Rate, Row, Tag, Tooltip } from "antd";
 import Link from "next/link";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromBuilder } from "@/redux/features/pcBuilder/pcBuilderSlice";
 
 const PcBuilderPage = () => {
   const { components } = useSelector((state) => state.pcBuilder);
   console.log(components);
+
+  const dispatch = useDispatch();
 
   const cpu = components.find(
     (component) => component.category === "Processor"
@@ -15,11 +18,43 @@ const PcBuilderPage = () => {
   return (
     <div>
       <Row>
-        <Col className="w-full mb-10">
+        <Col className="w-full mb-10 flex justify-center">
           <List
+            className="w-3/5 "
             header={<div className="font-semibold ">Pc Builder</div>}
             itemLayout="horizontal"
           >
+            <List.Item
+              className="bg-slate-50 mb-2 p-2 "
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
+              />
+            </List.Item>
             <List.Item
               className="bg-slate-50 mb-2 p-2"
               actions={[
@@ -36,6 +71,7 @@ const PcBuilderPage = () => {
                   type="text"
                   key="list-loadmore-edit"
                   className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
                 >
                   Remove
                 </Button>,
@@ -44,14 +80,165 @@ const PcBuilderPage = () => {
               <List.Item.Meta
                 title={<span>CPU / Processor </span>}
                 description={
-                  <span>{cpu ? cpu?.product_name : "Component Name"}</span>
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
                 }
               />
             </List.Item>
-            <List.Item className="bg-slate-50 mb-2 p-2">
+            <List.Item
+              className="bg-slate-50 mb-2 p-2"
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
               <List.Item.Meta
-                title={<span>MotherBoard </span>}
-                description={<span>Component Name</span>}
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
+              />
+            </List.Item>
+            <List.Item
+              className="bg-slate-50 mb-2 p-2"
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
+              />
+            </List.Item>
+            <List.Item
+              className="bg-slate-50 mb-2 p-2"
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
+              />
+            </List.Item>
+            <List.Item
+              className="bg-slate-50 mb-2 p-2"
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
+              />
+            </List.Item>
+            <List.Item
+              className="bg-slate-50 mb-2 p-2"
+              actions={[
+                <Link href="/pcbuilder/cpu" key="list-loadmore-edit">
+                  <Button
+                    type="text"
+                    key="list-loadmore-edit"
+                    className={cpu ? "text-yellow-500" : "text-green-500"}
+                  >
+                    {cpu ? "Change" : "Choose"}
+                  </Button>
+                </Link>,
+                <Button
+                  type="text"
+                  key="list-loadmore-edit"
+                  className="text-red-500"
+                  onClick={() => dispatch(removeFromBuilder(cpu))}
+                >
+                  Remove
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<span>CPU / Processor </span>}
+                description={
+                  <span>
+                    {cpu ? cpu?.product_name : "No Component Selected"}
+                  </span>
+                }
               />
             </List.Item>
           </List>
--- src/redux/features/pcBuilder/pcBuilderSlice.js
+++ src/redux/features/pcBuilder/pcBuilderSlice.js
@@ -10,15 +10,25 @@ const pcBuilderSlice = createSlice({
   initialState,
   reducers: {
     addToBuilder: (state, action) => {
-      const exists = state.components.find(
-        (component) => component._id === action.payload._id
+      const index = state.components.findIndex(
+        (component) => component.category === action.payload.category
       );
-      if (!exists) {
+
+      if (index !== -1) {
+        // If a component of the same category exists, replace it
+        state.components[index] = action.payload;
+      } else {
+        // If a component of the same category doesn't exist, add the new component
         state.components.push(action.payload);
       }
+    },
+    removeFromBuilder: (state, action) => {
+      state.components = state.components.filter(
+        (component) => component._id !== action.payload._id
+      );
     },
   },
 });
 
-export const { addToBuilder } = pcBuilderSlice.actions;
+export const { addToBuilder, removeFromBuilder } = pcBuilderSlice.actions;
 export default pcBuilderSlice.reducer;
